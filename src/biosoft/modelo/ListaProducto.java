415b5e3a2f7c9410773710d20fc16e7f4985108d
Franco Leonel Martinez <fm3448530@gmail.com>
2019-10-16 11:01:25 -0300

    Modificación clase Lista Producto

--- src/biosoft/modelo/ListaProducto.java
+++ src/biosoft/modelo/ListaProducto.java
@@ -8,9 +8,19 @@ public class ListaProducto {
     private ArrayList listaProducto;
     private Producto producto;
 
-    public ListaProducto(ArrayList listaProducto) {
+    public ListaProducto(ArrayList listaProducto, Producto producto) {
         this.listaProducto = listaProducto;
+        this.producto = producto;
     }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
 
     public ArrayList getListaProducto() {
         return listaProducto;
