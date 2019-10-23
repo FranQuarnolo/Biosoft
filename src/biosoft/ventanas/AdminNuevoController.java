c132ac92080d57003fa3bc27d287871a7ff8c124
Suarez nestor <suareznestorandres@hotmail.com>
2019-10-23 10:37:30 -0300

    Funcionalidad de agregar producto

--- src/biosoft/ventanas/AdminNuevoController.java
+++ src/biosoft/ventanas/AdminNuevoController.java
@@ -1,8 +1,13 @@
 
 package biosoft.ventanas;
 
+import biosoft.modelo.ControladorProducto;
+import biosoft.modelo.Producto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
+import org.controlsfx.dialog.Dialogs;
 
 /**
  *
@@ -10,12 +15,55 @@ import javafx.fxml.FXML;
  */
 public class AdminNuevoController {
     
+    @FXML
+    private TextField tNombre;
+    @FXML
+    private TextField tTipo;   
     
-    
-    
-    
-     @FXML
+    ControladorProducto controladorProducto = new ControladorProducto();
+    Producto producto = new Producto();
+    @FXML
     private void apretarSalir(ActionEvent event){
         System.exit(0); 
+    }
+    
+    @FXML
+    private void apretarNuevo(ActionEvent event){
+        if (siEsInvalido()) {
+            try{
+           
+               // producto.setDni(Integer.parseInt(tDni.getText()));
+                producto.setNombre(tNombre.getText());                
+                producto.setTipo(tTipo.getText());
+                
+                controladorProducto.insertarSQL(producto);
+                
+            }catch(NumberFormatException e){
+                    JOptionPane.showMessageDialog(null, "Esta ingresando valores no numericos en campos numericos\n "+e); 
+            }
+        }
+    }
+    
+      public boolean siEsInvalido() {
+        String errorMessage = "";
+
+        if (tNombre.getText() == null || tNombre.getText().length() == 0) {
+            errorMessage += "Nombre no valido!\n";
+        }
+        if (tTipo.getText() == null || tTipo.getText().length() == 0) {
+            errorMessage += "Tipo Invalido!\n";
+        }
+        
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Muestra el mensaje de error
+            Dialogs.create()
+                .title("Campo invalido")
+                .masthead("Revise los campos invalidos")
+                .message(errorMessage)
+                .showError();
+            return false;
+        }
     }
 }
