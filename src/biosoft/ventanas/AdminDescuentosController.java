aa570178c7e7aff771b258b4923f19dac0c8f52e
franquarnolo <fran_q98@hotmail.com>
2019-11-21 09:42:22 -0300

    Alerta de Seleccionar uno en Editar

--- src/biosoft/ventanas/AdminDescuentosController.java
+++ src/biosoft/ventanas/AdminDescuentosController.java
@@ -41,7 +41,7 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
     private TableColumn<Cantidad, String> nombreCant;
     @FXML
     private TableColumn<Cantidad, String> descuentoCant;
-    
+
     //Declaraciones Lista Destino
     @FXML
     private TableView<Destino> listaDest;
@@ -51,7 +51,7 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
     private TableColumn<Destino, String> nombreDest;
     @FXML
     private TableColumn<Destino, String> descuentoDest;
-    
+
     //Declaraciones Lista Origen
     @FXML
     private TableView<Origen> listaOrig;
@@ -61,7 +61,7 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
     private TableColumn<Origen, String> nombreOrig;
     @FXML
     private TableColumn<Origen, String> descuentoOrig;
-    
+
     //Declaraciones Lista TiempoEntrega
     @FXML
     private TableView<TiempoEntrega> listaTiempo;
@@ -71,7 +71,7 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
     private TableColumn<TiempoEntrega, String> nombreTiempoEntrega;
     @FXML
     private TableColumn<TiempoEntrega, String> descuentoTiempoEntrega;
-    
+
     //Declaraciones Lista Plazo de Pago
     @FXML
     private TableView<PlazoPago> listaPlazo;
@@ -81,16 +81,17 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
     private TableColumn<PlazoPago, String> nombrePlazo;
     @FXML
     private TableColumn<PlazoPago, String> descuentoPlazo;
-    
+
     private ObservableList<Cantidad> listaCantidad;
     private ObservableList<Destino> listaDestino;
     private ObservableList<Origen> listaOrigen;
     private ObservableList<TiempoEntrega> listaTiempoEntrega;
     private ObservableList<PlazoPago> listaPlazoPago;
-    
-    public AdminDescuentosController(){
-        
+
+    public AdminDescuentosController() {
+
     }
+
     //INICIALIZANDO CAMPOS
     @Override
     public void initialize(URL location, ResourceBundle rb) {
@@ -102,7 +103,7 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
         idCant.setCellValueFactory(new PropertyValueFactory<>("idCantidad"));
         nombreCant.setCellValueFactory(new PropertyValueFactory<>("nombre"));
         descuentoCant.setCellValueFactory(new PropertyValueFactory<>("descuento"));
-        
+
         //Aca carga los de la lista de DESTINO
         listaDestino = FXCollections.observableArrayList();
         db.llenarDestino(db.getConexion(), listaDestino);
@@ -110,7 +111,7 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
         idDest.setCellValueFactory(new PropertyValueFactory<>("idDestino"));
         nombreDest.setCellValueFactory(new PropertyValueFactory<>("nombre"));
         descuentoDest.setCellValueFactory(new PropertyValueFactory<>("descuento"));
-        
+
         //Aca carga los de la lista de ORIGEN
         listaOrigen = FXCollections.observableArrayList();
         db.llenarOrigen(db.getConexion(), listaOrigen);
@@ -118,7 +119,7 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
         idOrig.setCellValueFactory(new PropertyValueFactory<>("idOrigen"));
         nombreOrig.setCellValueFactory(new PropertyValueFactory<>("nombre"));
         descuentoOrig.setCellValueFactory(new PropertyValueFactory<>("descuento"));
-        
+
         //Aca carga los de la lista de TIEMPO DE ENTREGA
         listaTiempoEntrega = FXCollections.observableArrayList();
         db.llenarTiempoEntrega(db.getConexion(), listaTiempoEntrega);
@@ -126,8 +127,8 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
         idTiempoEntrega.setCellValueFactory(new PropertyValueFactory<>("idTiempodeEntrega"));
         nombreTiempoEntrega.setCellValueFactory(new PropertyValueFactory<>("nombre"));
         descuentoTiempoEntrega.setCellValueFactory(new PropertyValueFactory<>("descuento"));
-        
-         //Aca carga los de la lista de PLAZO DE PAGO
+
+        //Aca carga los de la lista de PLAZO DE PAGO
         listaPlazoPago = FXCollections.observableArrayList();
         db.llenarPlazoPago(db.getConexion(), listaPlazoPago);
         listaPlazo.setItems(listaPlazoPago);
@@ -135,75 +136,93 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
         nombrePlazo.setCellValueFactory(new PropertyValueFactory<>("nombre"));
         descuentoPlazo.setCellValueFactory(new PropertyValueFactory<>("descuento"));
     }
-    //Metodo actualizar listass
-     @FXML
-     public void apretarReload(ActionEvent event) {
-        listaCantidad = FXCollections.observableArrayList();
-        ControladorBaseDatosFx db = new ControladorBaseDatosFx();
-        db.llenarCantidad(db.getConexion(), listaCantidad);
-        listaCant.setItems(listaCantidad);
-        idCant.setCellValueFactory(new PropertyValueFactory<>("idCantidad"));
-        nombreCant.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        descuentoCant.setCellValueFactory(new PropertyValueFactory<>("descuento"));
-        
-        //Aca carga los de la lista de DESTINO
-        listaDestino = FXCollections.observableArrayList();
-        db.llenarDestino(db.getConexion(), listaDestino);
-        listaDest.setItems(listaDestino);
-        idDest.setCellValueFactory(new PropertyValueFactory<>("idDestino"));
-        nombreDest.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        descuentoDest.setCellValueFactory(new PropertyValueFactory<>("descuento"));
-        
-        //Aca carga los de la lista de ORIGEN
-        listaOrigen = FXCollections.observableArrayList();
-        db.llenarOrigen(db.getConexion(), listaOrigen);
-        listaOrig.setItems(listaOrigen);
-        idOrig.setCellValueFactory(new PropertyValueFactory<>("idOrigen"));
-        nombreOrig.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        descuentoOrig.setCellValueFactory(new PropertyValueFactory<>("descuento"));
-        
-        //Aca carga los de la lista de TIEMPO DE ENTREGA
-        listaTiempoEntrega = FXCollections.observableArrayList();
-        db.llenarTiempoEntrega(db.getConexion(), listaTiempoEntrega);
-        listaTiempo.setItems(listaTiempoEntrega);
-        idTiempoEntrega.setCellValueFactory(new PropertyValueFactory<>("idTiempodeEntrega"));
-        nombreTiempoEntrega.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        descuentoTiempoEntrega.setCellValueFactory(new PropertyValueFactory<>("descuento"));
-        
-         //Aca carga los de la lista de PLAZO DE PAGO
-        listaPlazoPago = FXCollections.observableArrayList();
-        db.llenarPlazoPago(db.getConexion(), listaPlazoPago);
-        listaPlazo.setItems(listaPlazoPago);
-        idPlazo.setCellValueFactory(new PropertyValueFactory<>("idPlazoDePago"));
-        nombrePlazo.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        descuentoPlazo.setCellValueFactory(new PropertyValueFactory<>("descuento"));
-    }
-   
+
+//    //Metodo actualizar listass
+//    @FXML
+//    public void apretarReload(ActionEvent event) {
+//        listaCantidad = FXCollections.observableArrayList();
+//        ControladorBaseDatosFx db = new ControladorBaseDatosFx();
+//        db.llenarCantidad(db.getConexion(), listaCantidad);
+//        listaCant.setItems(listaCantidad);
+//        idCant.setCellValueFactory(new PropertyValueFactory<>("idCantidad"));
+//        nombreCant.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+//        descuentoCant.setCellValueFactory(new PropertyValueFactory<>("descuento"));
+//
+//        //Aca carga los de la lista de DESTINO
+//        listaDestino = FXCollections.observableArrayList();
+//        db.llenarDestino(db.getConexion(), listaDestino);
+//        listaDest.setItems(listaDestino);
+//        idDest.setCellValueFactory(new PropertyValueFactory<>("idDestino"));
+//        nombreDest.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+//        descuentoDest.setCellValueFactory(new PropertyValueFactory<>("descuento"));
+//
+//        //Aca carga los de la lista de ORIGEN
+//        listaOrigen = FXCollections.observableArrayList();
+//        db.llenarOrigen(db.getConexion(), listaOrigen);
+//        listaOrig.setItems(listaOrigen);
+//        idOrig.setCellValueFactory(new PropertyValueFactory<>("idOrigen"));
+//        nombreOrig.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+//        descuentoOrig.setCellValueFactory(new PropertyValueFactory<>("descuento"));
+//
+//        //Aca carga los de la lista de TIEMPO DE ENTREGA
+//        listaTiempoEntrega = FXCollections.observableArrayList();
+//        db.llenarTiempoEntrega(db.getConexion(), listaTiempoEntrega);
+//        listaTiempo.setItems(listaTiempoEntrega);
+//        idTiempoEntrega.setCellValueFactory(new PropertyValueFactory<>("idTiempodeEntrega"));
+//        nombreTiempoEntrega.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+//        descuentoTiempoEntrega.setCellValueFactory(new PropertyValueFactory<>("descuento"));
+//
+//        //Aca carga los de la lista de PLAZO DE PAGO
+//        listaPlazoPago = FXCollections.observableArrayList();
+//        db.llenarPlazoPago(db.getConexion(), listaPlazoPago);
+//        listaPlazo.setItems(listaPlazoPago);
+//        idPlazo.setCellValueFactory(new PropertyValueFactory<>("idPlazoDePago"));
+//        nombrePlazo.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+//        descuentoPlazo.setCellValueFactory(new PropertyValueFactory<>("descuento"));
+//    }
 
     //Boton Editar
     @FXML
     private void apretarEditar(ActionEvent event) {
-       try {
-           int VerificadorDeDescuento=0;
-            if(listaCant.getSelectionModel().getSelectedItem()!=null){
-                VerificadorDeDescuento=1;
+        try {
+            int VerificadorDeDescuento = 0;
+            if (listaCant.getSelectionModel().getSelectedItem() != null) {
+                VerificadorDeDescuento = 1;
 
-            }else if(listaDest.getSelectionModel().getSelectedItem()!=null){
-                VerificadorDeDescuento=2;
+            } else if (listaDest.getSelectionModel().getSelectedItem() != null) {
+                VerificadorDeDescuento = 2;
 
-            }else if(listaOrig.getSelectionModel().getSelectedItem()!=null){
-                VerificadorDeDescuento=3;
+            } else if (listaOrig.getSelectionModel().getSelectedItem() != null) {
+                VerificadorDeDescuento = 3;
 
-            }else if(listaTiempo.getSelectionModel().getSelectedItem()!=null){
-                VerificadorDeDescuento=4;
-  
-            }else if(listaPlazo.getSelectionModel().getSelectedItem()!=null){
-                VerificadorDeDescuento=5;
+            } else if (listaTiempo.getSelectionModel().getSelectedItem() != null) {
+                VerificadorDeDescuento = 4;
 
+            } else if (listaPlazo.getSelectionModel().getSelectedItem() != null) {
+                VerificadorDeDescuento = 5;
+            }else{
+                 try {
+                    //Cargo el archivo fxml
+                    FXMLLoader FXMLLoader3 = new FXMLLoader(getClass().getResource("SeleccioneElemento.fxml"));
+                    Parent root8 = (Parent) FXMLLoader3.load();
+                    Stage seleccion = new Stage();
+                    seleccion.setScene(new Scene(root8));
+                    seleccion.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("biosoft/images/alert1.png")));
+                    seleccion.setTitle("No Seleccion!");
+                    seleccion.initStyle(StageStyle.UNDECORATED);
+                    seleccion.initModality(Modality.APPLICATION_MODAL);
+                    System.out.println("Seleccione un elemento por favor");
+                    //Inicio la ventana
+                    seleccion.showAndWait();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+            
             System.out.println(VerificadorDeDescuento);
-            
-            
+            if(VerificadorDeDescuento!=0){
+                
             //Cargo el archivo fxml
             FXMLLoader FXMLLoader7 = new FXMLLoader(getClass().getResource("AdminEditarDescuentos.fxml"));
             Parent root8 = (Parent) FXMLLoader7.load();
@@ -216,52 +235,51 @@ public class AdminDescuentosController extends ControladorBaseDatosFx implements
             System.out.println("Editando campo seleccionado");
             //Inicio la ventana
             AdminEditarDescuentosController editarDes = FXMLLoader7.getController();
-            switch(VerificadorDeDescuento){
+            switch (VerificadorDeDescuento) {
                 case 1:
-                    
+
                     editarDes.MetodoCantidad(listaCant.getSelectionModel().getSelectedItem());
-                                    listaCant.getItems().clear();
-                ControladorBaseDatosFx db = new ControladorBaseDatosFx();
-                db.llenarCantidad(db.getConexion(), listaCantidad);
-                listaCant.setItems(listaCantidad);
+                    listaCant.getItems().clear();
+                    ControladorBaseDatosFx db = new ControladorBaseDatosFx();
+                    db.llenarCantidad(db.getConexion(), listaCantidad);
+                    listaCant.setItems(listaCantidad);
                     break;
-                case 2: 
+                case 2:
                     editarDes.MetodoDestino(listaDest.getSelectionModel().getSelectedItem());
-                                    listaDest.getItems().clear();
-                ControladorBaseDatosFx db1 = new ControladorBaseDatosFx();
-                db1.llenarDestino(db1.getConexion(), listaDestino);
-                listaDest.setItems(listaDestino);
+                    listaDest.getItems().clear();
+                    ControladorBaseDatosFx db1 = new ControladorBaseDatosFx();
+                    db1.llenarDestino(db1.getConexion(), listaDestino);
+                    listaDest.setItems(listaDestino);
                     break;
                 case 3:
                     editarDes.MetodoOrigen(listaOrig.getSelectionModel().getSelectedItem());
-                                    listaOrig.getItems().clear();
-                ControladorBaseDatosFx db2 = new ControladorBaseDatosFx();
-                db2.llenarOrigen(db2.getConexion(), listaOrigen);
-                listaOrig.setItems(listaOrigen);
+                    listaOrig.getItems().clear();
+                    ControladorBaseDatosFx db2 = new ControladorBaseDatosFx();
+                    db2.llenarOrigen(db2.getConexion(), listaOrigen);
+                    listaOrig.setItems(listaOrigen);
                     break;
                 case 4:
                     editarDes.MetodoTiempoEntrega(listaTiempo.getSelectionModel().getSelectedItem());
-                                  listaTiempo.getItems().clear();
-                ControladorBaseDatosFx db3 = new ControladorBaseDatosFx();
-                db3.llenarTiempoEntrega(db3.getConexion(), listaTiempoEntrega);
-                listaTiempo.setItems(listaTiempoEntrega);
+                    listaTiempo.getItems().clear();
+                    ControladorBaseDatosFx db3 = new ControladorBaseDatosFx();
+                    db3.llenarTiempoEntrega(db3.getConexion(), listaTiempoEntrega);
+                    listaTiempo.setItems(listaTiempoEntrega);
                     break;
                 case 5:
                     editarDes.MetodoPlazoPago(listaPlazo.getSelectionModel().getSelectedItem());
-                                    listaPlazo.getItems().clear();
-                ControladorBaseDatosFx db4 = new ControladorBaseDatosFx();
-                db4.llenarPlazoPago(db4.getConexion(), listaPlazoPago);
-                listaPlazo.setItems(listaPlazoPago);
+                    listaPlazo.getItems().clear();
+                    ControladorBaseDatosFx db4 = new ControladorBaseDatosFx();
+                    db4.llenarPlazoPago(db4.getConexion(), listaPlazoPago);
+                    listaPlazo.setItems(listaPlazoPago);
                     break;
             }
             editar.showAndWait();
-            
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    
+
     //Boton Cerrar Sesion
     @FXML
     private void apretarAtras(ActionEvent event) throws IOException {
